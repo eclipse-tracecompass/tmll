c60c2a4f66664837a2a2c355a7463a82accfa008
Kaveh Shahedi <kaveh.shahedi@ericsson.com>
2024-08-27 18:09:59 -0400

    [UPDATE] Logger system replace with Loguru

--- tmll/common/services/logger.py
+++ tmll/common/services/logger.py
@@ -4,48 +4,30 @@ This module contains the Logger class which is used to log messages to the conso
 Methods:
     - info: Log an info message to the console.
     - error: Log an error message to the console.
+    - warning: Log a warning message to the console.
 """
 
-import builtins
-from functools import wraps
+from loguru import logger
+import sys
 
-from tmll.common.utils.colors import ConsoleColors
-
-"""
-In this code block, we want to disable the print function globally, except for the Logger class.
-"""
-_original_print = builtins.print
-
-def enable_printing(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        builtins.print = _original_print
-        try:
-            return func(*args, **kwargs)
-        finally:
-            builtins.print = lambda *args, **kwargs: None
-    return wrapper
-builtins.print = lambda *args, **kwargs: None
 
 class Logger:
+    logger.remove()
+    logger.add(sys.stdout, colorize=True, format="{message}")
+
     def __init__(self, name: str, verbose: bool = True) -> None:
         self.name = name
         self.verbose = verbose
 
-    def _format_message(self, level: str, msg: str) -> str:
-        return (f"{ConsoleColors.BOLD}{level}{ConsoleColors.ENDC} "
-                f"{ConsoleColors.OKGREEN}{self.name}:{ConsoleColors.ENDC} {msg}")
+    def __log_message(self, tag: str, message: str, color: str) -> None:
+        message = f"<bold><{color}>[{tag}]</{color}> <green>{self.name}:</green></bold> {message}"
+        logger.opt(colors=True).info(message)
 
-    def info(self, msg: str) -> None:
-        self._log_message(f"{ConsoleColors.OKBLUE}[INFO]{ConsoleColors.ENDC}", msg)
+    def info(self, message: str) -> None:
+        self.__log_message("INFO", message, "blue")
 
-    def error(self, msg: str) -> None:
-        self._log_message(f"{ConsoleColors.FAIL}[ERROR]{ConsoleColors.ENDC}", msg)
+    def error(self, message: str) -> None:
+        self.__log_message("ERROR", message, "red")
 
-    def warning(self, msg: str) -> None:
-        self._log_message(f"{ConsoleColors.WARNING}[WARNING]{ConsoleColors.ENDC}", msg)
-
-    @enable_printing
-    def _log_message(self, level: str, msg: str) -> None:
-        if self.verbose:
-            print(self._format_message(level, msg))
+    def warning(self, message: str) -> None:
+        self.__log_message("WARNING", message, "yellow")
